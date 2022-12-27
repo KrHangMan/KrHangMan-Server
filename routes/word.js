e6cd42061c1dd5ff7c869be721fd105ee0215cc0
choijaeho <dhqkgbs@naver.com>
2022-12-27 21:19:04 +0900

    feat : develop spread 10 words api in word.js

--- routes/word.js
+++ routes/word.js
@@ -1,44 +1,73 @@
 const express = require('express');
 const router = express.Router();
-module.exports = router;
+const conn = require('../config/db.js');
+const mysql = require('mysql2/promise');
+const Hangul = require('hangul-js');    // 한글 자모자 분리 library
 
-/** spread 10 words (자모자 형식으로) */
+/** spread 10 words : 10개 단어 보내줌  */
 router.get('/', async (req, res, next) => {      
-    console.log("spread 10 words (자모자 형식으로)");
+    try {  
+        var connection = await mysql.createConnection(conn.real); // DB 커넥션 생성
+        await connection.connect();   // DB 접속
+
+        const username      = req.params.username;
+        const correct_cnt   = req.body.correct_cnt;
+        const query         = `SELECT WORD, MEAN FROM WORDS ORDER BY RAND() LIMIT 3;`; //갯수 임의로 지정 할 수 있도록? 
+        const res_data      = await connection.query(query);
+
+        if(res_data == null || undefined){
+            return res.status(404).json({
+                "code": 404,
+                "message": "Not found words"
+            });
+        }
+
+        let arr = new Array() ;
+        for(let i in res_data[0]){
+            let data = new Object() ;
+			
+			data.WORD = res_data[0][i].WORD;
+			data.MEAN = res_data[0][i].MEAN;
+            data.SPELL = Hangul.disassemble(res_data[0][i].WORD); 
+
+            arr.push(data);
+        }
+        let items = JSON.stringify(arr) ;
+        console.log(items);
+
+        // res_data 
+        return res.status(200).json({
+            items,
+            "code": 200,
+            "message": "spread 10 words success"
+        });   
+    } catch (error) {
+        console.error(error);  
+        res.status(500).json({
+            "code":500,
+            "message": "server error"
+        });
+    }
 
     /*
-        ## Request
-        
-        ## request 데이터
-        
-
+        ## request X
         ### 단어 10개 주는 방식 
-        1. users 마다의 sequence를 구해서 10개씩 보낸다.
-        2. 랜덤하게  10개를 보낸다 -> 중복 데이터 무시 
-        3. client 에서 paging 처리하듯이 10개씩 보낸다. 
-    
-
+        랜덤하게  10개를 보낸다 -> 중복 데이터 무시 
         ## Response
         {
-             "total" : "2432",
-             "start" : "1",
-             "num"   : "10",
              "items" : {
                 {
-                    "id"    : "1",
                     "spell" : [ㄱ,ㅏ,ㄴ,ㄷ,ㅏ],
                     "word"  : "간다",
                     "mean"  : "간다는뜻"
                 },
                 {
-                    "id"    : "2",
                     "spell" : [ㄱ,ㅏ,ㄴ,ㄷ,ㅏ],
                     "word"  : "간다",
                     "mean"  : "간다는뜻"
                 },
                 ...
                 {
-                    "id"    : "10",
                     "spell" : [ㄱ,ㅏ,ㄴ,ㄷ,ㅏ],
                     "word"  : "간다",
                     "mean"  : "간다는뜻"
@@ -49,3 +78,6 @@ router.get('/', async (req, res, next) => {
 
     */
 });
+
+
+module.exports = router;
